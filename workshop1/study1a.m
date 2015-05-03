562658b05eaf351c648a3c7bf3f44fe22e843528
Ang Li <angleedomain@gmail.com>
2015-05-03 16:19:59 +1000

    rename

--- workshop1/study1a.m
+++ workshop1/study1a.m
@@ -5,35 +5,30 @@ SD2 = 2.2 + 1.32i;
 V1 = 0.98;
 P21 = 1.1;
 
-Y(1, 1) = 1 / Z_line + (Csh * 100 * pi / 2) * 1i;
-Y(1, 2) = -1 / Z_line;
-Y(2, 1) = -1 / Z_line;
-Y(2, 2) = 1 / Z_line + (Csh * 100 * pi / 2) * 1i;
+Y11 = 1 / Z_line + (Csh * 100 * pi / 2) * 1i;
+Y12 = -1 / Z_line;
+Y21 = -1 / Z_line;
+Y22 = 1 / Z_line + (Csh * 100 * pi / 2) * 1i;
 
 tolerrence = 1E-12;
 deltaV = 1;
-V2 = 1;
+index = 1;
+V2(index) = 1;
 
 while deltaV > tolerrence
-	I1 = Y(1, 1) * V1 + Y(1, 2) * V2;
-	S12 = V1 * conj(I1);
-
-	I2 = Y(2, 1) * V1 + Y(2, 2) * V2;
-	S21 = P21 + 1i * imag( V2 * conj(I2) );
-
-	V2plus1 = (1 / Y(2, 2)) * (conj(S21 / V2) - Y(2, 1) * V1);
-	V2plus1 = V2plus1 / abs(V2plus1);
-
-	theta = radtodeg(angle(V2plus1));
-
-	deltaV = abs(V2 - V2plus1);
-	
-	V2 = V2plus1;
+	S12(index) = V1 * conj(Y11 * V1 + Y12 * V2(index));
+	S21(index) = P21 + 1i * imag( V2(index) * conj(Y21 * V1 + Y22 * V2(index)) );
+	V2(index+1) = (1 / Y22) * (conj(S21(index) / V2(index)) - Y21 * V1);
+	thetaRadian = angle(V2(index+1));
+	thetaDegree = radtodeg(thetaRadian);
+	V2(index+1) = cos(thetaRadian) + 1i * sin(thetaRadian);
+	deltaV = abs(V2(index) - V2(index+1));
+	index = index + 1;
 end
 
 SG1 = SD1 + S12;
 SG2 = SD2 + S21;
 
-fprintf('SG1 = %f + j%f pu\n', real(SG1), imag(SG1));
-fprintf('SG2 = %f + j%f pu\n', real(SG2), imag(SG2));
-fprintf('Theta = %f degress \n', theta);
+fprintf('SG1 = %f + j%f pu\n', real(SG1(index-1)), imag(SG1(index-1)));
+fprintf('SG2 = %f + j%f pu\n', real(SG2(index-1)), imag(SG2(index-1)));
+fprintf('Theta = %f degress \n', thetaDegree);
