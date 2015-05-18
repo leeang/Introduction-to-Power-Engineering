f36c97ea725ed89406bebbd83e1613249fe6990c
Ang Lee <angleedomain@gmail.com>
2015-05-18 14:08:16 +1000

    update calc

--- workshop2/calc.m
+++ workshop2/calc.m
@@ -36,20 +36,22 @@ X_1 = X_eq - X_2 * alpha^2;
 
 %%Q3
 V_1 = 230;
-W_0_Q3 = 235;
+W_0_Q3 = 435;
 A_1 = 2;
 V_2_Q2 = 112;
 
-I_1_prime = (V_1 - alpha * V_2_Q2) / sqrt(R_eq^2 + X_eq^2);
-P_v = I_1_prime^2 * R_eq;
-P_loss = W_0_Q1 + P_v;
-eta = (W_0_Q3 - P_loss) / W_0_Q3;
+AA = R_eq;
+BB = alpha * V_2_Q2;
+CC = W_0_Q1 - W_0_Q3;
+
+I_1_prime = (-BB + sqrt(BB^2 - 4 * AA * CC)) / (2 * AA);
+P_load = V_2_Q2 * alpha * I_1_prime;
+eta = P_load / W_0_Q3;
 
 R_load = V_2_Q2 / (alpha * I_1_prime);
 
 I_1_prime_max = sqrt(W_0_Q1 / R_eq);
 I_2_max = alpha * I_1_prime_max;
-
 P_out = I_2_max^2 * R_load;
 eta_max = P_out / (P_out + 2 * W_0_Q1);
 
